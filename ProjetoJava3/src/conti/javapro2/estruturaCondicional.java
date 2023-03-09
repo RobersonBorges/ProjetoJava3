5007c1261bc0e611d7dda41b1c8ef60e2f164db3
RobersonBorges <robersonfborges@gmail.com>
2023-03-09 18:49:37 -0300

    codigo com sisteminha de calculo de media, exercicio de estrutura
    condicional simples e aninhada

---
+++ ProjetoJava3/src/conti/javapro2/estruturaCondicional.java
@@ -0,0 +1,32 @@
+package conti.javapro2;
+
+import java.util.Scanner;
+
+public class estruturaCondicional {
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("****** SISTEMA DE CALCULO DE  MEDIA ALUNOS *******");
+		/* solicitando primeira nota*/
+		System.out.println("digite a primeira nota: ");
+		int nota1 = scanner.nextInt();
+		/*solicitando segunda nota*/
+		System.out.println("digite a segunda nota: ");
+		int nota2 = scanner.nextInt();
+		System.out.println("digite a terceira nota: ");
+		int nota3 = scanner.nextInt();
+		double mediaFinal = (nota1+nota2+nota3)/3;
+		
+		if (mediaFinal >= 7){
+			System.out.println("você está aprovado, e sua media e: " + mediaFinal);						
+	}	
+		else {
+		if(mediaFinal < 7 && mediaFinal >= 5) {
+		System.out.println("voce esta de recuperacao e sua media e: " +mediaFinal);}
+		else {
+			System.out.println("voce esta reprovado e sua media e: " +mediaFinal);
+		}
+		}
+		}
+}
+
