393ff80297916113718da1506f16558fcab1286a
RobersonBorges <robersonfborges@gmail.com>
2023-03-09 15:28:09 -0300

    criação do projeto3, continuação do 2

---
+++ ProjetoJava3/src/conti/javapro2/Teste.java
@@ -0,0 +1,13 @@
+package conti.javapro2;
+
+public class Teste {
+	public static void main(String[] args) {
+		int valor1 = 90;
+		int valor2 = 80;
+		int valor3 = 70;
+		double mediaFinal = (valor1+valor2+valor3)/4;
+		System.out.println("as notas foram: " +valor1+ "," +valor2+","+valor3);
+		System.out.println("e a media final é: " +mediaFinal);
+	}
+
+}
