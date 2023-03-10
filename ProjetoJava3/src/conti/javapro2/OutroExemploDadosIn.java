c32577138cd03eb88b51627d3ebb3c49d0104553
RobersonBorges <robersonfborges@gmail.com>
2023-03-09 21:08:36 -0300

    add comentario a exemplo de entrada de dados

--- ProjetoJava3/src/conti/javapro2/OutroExemploDadosIn.java
+++ ProjetoJava3/src/conti/javapro2/OutroExemploDadosIn.java
@@ -1,17 +1,18 @@
 package conti.javapro2;
 
 import javax.swing.JOptionPane;
-
+/* a classe JOptionPane é melhor para ser usada em grafico.*/
 public class OutroExemploDadosIn {
 	public static void main(String[]args) {
 		String carros = JOptionPane.showInputDialog("digite o tanto de carros: ");
 		String pessoas = JOptionPane.showInputDialog("digite o tanto de pessoas: ");
-		
+		/* as duas proximas linhas, convertem a entrada de string acima, para 
+		   numero inteiro.*/
 		int carroNumero = (int) Double.parseDouble(carros);
 		int pessoasNumero = (int) Double.parseDouble(pessoas);
 		
 		double divisao = carroNumero / pessoasNumero;
-		System.out.println("a divisao deu: " +divisao);
+		JOptionPane.showMessageDialog(null, "a divisao deu: " +divisao);
 	}
 
 }
