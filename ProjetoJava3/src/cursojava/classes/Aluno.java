54d746905fccc625aeea2bb48f2cd5763e6f2e5c
RobersonBorges <robersonfborges@gmail.com>
2023-03-10 21:41:59 -0300

    criando Classe Aluno para projeto

---
+++ ProjetoJava3/src/cursojava/classes/Aluno.java
@@ -0,0 +1,17 @@
+package cursojava.classes;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String genero;
+	String registroGeral;
+	String cpf;
+	String nomeMae;
+	String nomePai;
+	String serieMatriculada;
+	String datadaMatricula;
+	String nomeEscola;
+	
+	
+}
---
+++ ProjetoJava3/src/cursojava/classes/ClasseExecutavel.java
@@ -0,0 +1,11 @@
+package cursojava.classes;
+
+import cursojava.classes.Aluno;
+
+public class ClasseExecutavel {
+	
+	public static void main (String[] args) {
+		/* Objeto sendo criado*/
+		Aluno aluno1 = new Aluno();
+	}
+}
