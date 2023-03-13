2a04b23d7ab0c325542fa0240fed596e655cc829
RobersonBorges <robersonfborges@gmail.com>
2023-03-13 20:56:15 -0300

    instanciando objetos e passando referencia

--- ProjetoJava3/src/cursojava/classes/ClasseExecutavel.java
+++ ProjetoJava3/src/cursojava/classes/ClasseExecutavel.java
@@ -6,6 +6,13 @@ public class ClasseExecutavel {
 	
 	public static void main (String[] args) {
 		/* Objeto sendo criado*/
-		Aluno aluno1 = new Aluno();
+		
+		/* o new Aluno(); é uma instancia (criação do objeto)*/
+		/* o aluno1 é uma referencia para o objeto aluno*/
+		Aluno aluno1 = new Aluno("doutor", 33);
+		Aluno aluno2 = new Aluno();
+		aluno2.nome = "roberson";
+		
+		
 	}
 }
