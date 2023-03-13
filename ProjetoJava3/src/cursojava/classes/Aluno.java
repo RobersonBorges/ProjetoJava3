4fb96775115faeb03811d2609b5a17f1e79090da
RobersonBorges <robersonfborges@gmail.com>
2023-03-13 20:56:03 -0300

    criando construtores

--- ProjetoJava3/src/cursojava/classes/Aluno.java
+++ ProjetoJava3/src/cursojava/classes/Aluno.java
@@ -1,7 +1,7 @@
 package cursojava.classes;
 
 public class Aluno {
-	
+	/* atributos do aluno, caracteristicas*/
 	String nome;
 	int idade;
 	String genero;
@@ -13,5 +13,11 @@ public class Aluno {
 	String datadaMatricula;
 	String nomeEscola;
 	
-	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+	public Aluno () {
+		
+	}
 }
