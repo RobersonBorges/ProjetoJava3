4eee7288ff3390d2739a26e070463a0795ff9dac
RobersonBorges <robersonfborges@gmail.com>
2023-03-13 21:19:18 -0300

    todos os set and get criados

--- ProjetoJava3/src/cursojava/classes/Aluno.java
+++ ProjetoJava3/src/cursojava/classes/Aluno.java
@@ -30,5 +30,60 @@ public class Aluno {
 	
 	public String getNome() {
 		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNomeMae() {
+		return nomeMae;
+	}
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	public String getNomePai() {
+		return nomePai;
+	}
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+	public String getSerieMatriculada() {
+		return serieMatriculada;
+	}
+	public void setSerieMatriculada(String serieMatriculada) {
+		this.serieMatriculada = serieMatriculada;
+	}
+	public String getDatadaMatricula() {
+		return datadaMatricula;
+	}
+	public void setDatadaMatricula(String datadaMatricula) {
+		this.datadaMatricula = datadaMatricula;
+	}
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
 	}	
+	
 }
