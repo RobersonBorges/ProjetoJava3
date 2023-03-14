632c0fc94addc03532fe2b14828015d5e57a4c43
RobersonBorges <robersonfborges@gmail.com>
2023-03-14 19:46:43 -0300

    criação dos atributos de notas e adição do metodo de calculo de media
    da classe aluno

--- ProjetoJava3/src/cursojava/classes/Aluno.java
+++ ProjetoJava3/src/cursojava/classes/Aluno.java
@@ -13,6 +13,11 @@ public class Aluno {
 	String datadaMatricula;
 	String nomeEscola;
 	
+	double nota1;
+	double nota2;
+	double nota3;
+	double nota4;
+	
 	public Aluno (String nomePadrao, int idadePadrao) {
 		nome = nomePadrao;
 		idade = idadePadrao;
@@ -84,6 +89,33 @@ public class Aluno {
 	}
 	public void setNomeEscola(String nomeEscola) {
 		this.nomeEscola = nomeEscola;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNota3() {
+		return nota3;
+	}
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+	public double getNota4() {
+		return nota4;
+	}
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
 	}	
-	
+	/* metodo que calcula media das notas, pegando os atributos na propria classe*/
+	public double getMediaNota() {
+		return (nota1+nota2+nota3+nota4)/4;
+	}
 }
